bd9ac7347d480134f8aa2621bedb18172efe8270
yashr <yashaswinirao12@gmail.com>
2025-11-11 19:11:04 +0530

    added super-user and db

--- laby/laby/settings.py
+++ laby/laby/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    
+    #external apps for support
+    'rest_framework',
+    'django_filters',
+    
+    #i need to add my aapps here
+    'equipment',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
